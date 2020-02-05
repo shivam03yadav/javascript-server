6af98aef7d2fa43dcf32106e26cfc877815953f4
shivam03yadav <shivam.yadav@successive.tech>
2020-02-05 21:13:47 +0530

    feat: add file

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -14,12 +14,12 @@ export default (config: any) => {
             const { in: reqMethod } = config[key];
             if (keys.includes('required') && config[key].required === true) {
                 if (req[reqMethod][key] === undefined || req[reqMethod][key] === null) {
-                    errorArray.push({ error: 'input is empty', message: `${key} is required`, status: 500, timeStamp: new Date()});
+                    errorArray.push({ error: 'input is empty', message: `${key} is required`, status: 400, timeStamp: new Date()});
                 }
             }
             if (keys.includes('regex')) {
                 if (!regex.test(req[reqMethod][key])) {
-                     errorArray.push({ error: 'Error found in', message: `${key} is invalid`, status: 500, timeStamp: new Date()});
+                     errorArray.push({ error: 'Error found in', message: `${key} is invalid`, status: 400, timeStamp: new Date()});
                 }
             }
          if (config[key].custom !== undefined) {
